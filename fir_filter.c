b0f08de14b77501f70ca295529eabbf1c3cd1081
Joe Gorse <jhgorse@gmail.com>
2016-04-10 21:26:25 -0400

    Naming conventions cleaned up for struct and data type.

--- fir_filter.c
+++ fir_filter.c
@@ -13,20 +13,20 @@
 #include <stdio.h>
 #include <math.h>
 
-typedef double fir_type_t;
+typedef double FIR_TYPE_DATA;
 
 /* Bring your own coefficients and data history buffers. */
-typedef struct {
+typedef struct fir_filter {
   // char name[32];      // Just in case you use these things
-  size_t      taps;   // How many taps we have
-  fir_type_t  *coef;  // Pointer to array of coefficients, taps long
-  fir_type_t  *x;     // History of inputs, taps long
-  size_t      ix;     // Iterator
-} fir_filter_t;
+  size_t        taps;   // How many taps we have
+  FIR_TYPE_DATA *coef;  // Pointer to array of coefficients, taps long
+  FIR_TYPE_DATA *x;     // History of inputs, taps long
+  size_t        ix;     // Iterator
+} FIR_FILTER;
 
 /* Give it a new value, get one in return. */
-fir_type_t fir_filter (fir_type_t new_value, fir_filter_t *fir) {
-  fir_type_t sum = 0;
+FIR_TYPE_DATA fir_filter (FIR_TYPE_DATA new_value, FIR_FILTER *fir) {
+  FIR_TYPE_DATA sum = 0;
   fir->x[fir->ix] = new_value;
   fir->ix = (fir->ix + 1) % fir->taps;  // Never exceed taps for index
 
@@ -37,23 +37,22 @@ fir_type_t fir_filter (fir_type_t new_value, fir_filter_t *fir) {
 
 int main () {
   // Filter init
-  fir_type_t fir_filter_coef[] = { 0, .5, .75, .5, 0 };
-  fir_type_t fir_history[]     = { 0,0,0,0,0 };
-  fir_filter_t a;
-  a.taps = sizeof(fir_filter_coef)/sizeof(fir_type_t);
+  FIR_TYPE_DATA fir_filter_coef[] = { 0, .5, .75, .5, 0 };
+  FIR_TYPE_DATA fir_history[]     = { 0,0,0,0,0 };
+  FIR_FILTER a;
+  a.taps = sizeof(fir_filter_coef)/sizeof(FIR_TYPE_DATA);
   a.x    = fir_history;
   a.coef = fir_filter_coef;
-  a.ix = 0;
+  a.ix   = 0;
 
   // Simulated data
-  fir_type_t input_data[] = { 0,1,0,0,0, 0,0,0,0,0 }; // filter looks good, check
-  // fir_type_t input_data[] = { 0,1,1,1,1, 1,1,1,1,1 };   // 1.75, check
+  int16_t input_data[] = { 0,1,0,0,0, 0,0,0,0,0 }; // filter looks good, check
+  // FIR_TYPE_DATA input_data[] = { 0,1,1,1,1, 1,1,1,1,1 };   // 1.75, check
   // double input_data[] = { 0,1,-1,1,-1, 1,-1,1,-1,1 };
-  size_t input_data_length = sizeof(input_data)/sizeof(double);
-  double out = -1;
+  size_t input_data_length = sizeof(input_data)/sizeof(int16_t);
 
   for (int i = 0; i < input_data_length; i++) {
-    printf("x(%d)=%f\tf(x)=%f\n", i, input_data[i],
+    printf("x(%d)=%d\tf(x)=%f\n", i, input_data[i],
       fir_filter(input_data[i], &a));
   }
 
